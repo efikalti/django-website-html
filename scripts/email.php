c08ab8a0ad6d7be59d7abb50326e9eb548d39c5e
Efi Kaltirimidou <efikalti@gmail.com>
2016-09-05 15:00:19 +0300

    email

--- scripts/email.php
+++ scripts/email.php
@@ -1,5 +1,5 @@
 <?php
 // Send
-$headers = "From: {$_POST['email']}" . "\r\n" .
+$headers = "From: {$_POST['email']}" . "\r\n";
 mail('efikalti@gmail.com', $_POST['subject'], $_POST['message'], $headers);
 ?>
